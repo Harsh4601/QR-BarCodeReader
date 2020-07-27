41f89d982bf563f349456498fa5653c2a96f1c14
“Harsh4601” <londhekarh4601@gmail.com>
2020-07-27 18:20:19 +0530

    Initial Commit

---
+++ QR-Reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QR-Reader
+//
+//  Created by Harsh Londhekar on 27/07/20.
+//  Copyright © 2020 Harsh Londhekar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
